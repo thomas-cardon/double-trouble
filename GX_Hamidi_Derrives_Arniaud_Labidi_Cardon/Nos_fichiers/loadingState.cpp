f090d7a8528abe24cf4bc6018e767462a6bdcacf
Thomas Cardon <thomas.cardon@pm.me>
2021-01-12 21:28:48 +0100

    LoadingState upgrade

--- GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/loadingState.cpp
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/loadingState.cpp
@@ -8,13 +8,6 @@ class LoadingState : public State {
 
         void load() override {}
 
-        int update(MinGL & window, int delta) override {
-            return 0;
-        }
-
-        void events(nsEvent::Event_t event) override {
-        }
-
         void render(MinGL & window) override {
             window.setBackgroundColor(nsGraphics::KCyan);
         }
