08ba40b34f82503ec6124ac1a6123938aa44d0c6
Thomas Cardon <thomas.cardon@pm.me>
2021-01-12 22:24:01 +0100

    Ajout système de Cooldowns pour faciliter la mesure du temps

---
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/cooldowns.cpp
@@ -0,0 +1,26 @@
+#include "cooldowns.h"
+
+void createCooldown(std::string id, int delay) {
+    cooldowns[id] = std::make_pair(0, delay);
+}
+
+void updateCooldowns(int delta) {
+    for (auto &cd : cooldowns)
+        cd.second.first += delta;
+}
+
+bool isCooldownOver(std::string id) {
+    if (cooldowns[id].first >= cooldowns[id].second) {
+        cooldowns[id].first = 0;
+        return true;
+    }
+    return false;
+}
+
+void setCooldownDelay(std::string id, int delay) {
+    cooldowns[id].second = delay;
+}
+
+void resetCooldowns() {
+    cooldowns.clear();
+}
---
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/cooldowns.h
@@ -0,0 +1,41 @@
+#ifndef COOLDOWNS_H
+#define COOLDOWNS_H
+
+#include <string.h>
+#include "type.h"
+
+std::map<std::string, Cooldown> cooldowns;
+
+/**
+ * @brief createCooldown
+ * @param id
+ * @param delay
+ */
+void createCooldown(std::string id, int delay);
+
+/**
+ * @brief updateCooldowns
+ * @param delta
+ */
+void updateCooldowns(int delta);
+
+/**
+ * @brief isCooldownOver
+ * @param id
+ * @return
+ */
+bool isCooldownOver(std::string id);
+
+/**
+ * @brief setCooldownDelay
+ * @param id
+ * @param delay
+ */
+void setCooldownDelay(std::string id, int delay);
+
+/**
+ * @brief resetCooldowns
+ */
+void resetCooldowns();
+
+#endif // COOLDOWNS_H
