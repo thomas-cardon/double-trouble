c8f80f731550f1ce6d351d42b3b4483897e435b5
Thomas Cardon <thomas.cardon@pm.me>
2021-01-17 12:43:09 +0100

    Stash items (WIP!)

---
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/fruit.cpp
@@ -0,0 +1,28 @@
+#include "fruit.h"
+#include <mingl/gui/sprite.h>
+#include <mingl/shape/circle.h>
+
+/**
+ *
+ * \file    fruit.cpp
+ * \author  Thomas Cardon
+ * \date    12 janvier 2020
+ * \version 1.0
+ * \brief   Method definitions for fruit.h
+ */
+
+using namespace nsGame;
+
+void Fruit::load() {
+
+}
+
+void Fruit::update(unsigned delta) {
+    sprite.setPosition(this->getCoordinates());
+}
+
+void Fruit::render(MinGL &window) {
+    std::cout << "render sprite" << std::endl;
+    window << nsShape::Circle(this->getCoordinates(), 2, nsGraphics::KCyan);
+    window << sprite;
+}
---
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/fruit.h
@@ -0,0 +1,60 @@
+#ifndef FRUIT_H
+#define FRUIT_H
+
+#include <mingl/mingl.h>
+#include <mingl/gui/sprite.h>
+#include <mingl/graphics/vec2d.h>
+
+#include "item.h"
+
+/**
+ * \file    fruit.h
+ * \author  Thomas Cardon
+ * \date    17 janvier 2020
+ * \version 1.0
+ * \brief   Fruit
+ */
+
+namespace nsGame {
+    /**
+     * @class Fruit
+     * @brief Defines Fruit
+     * @authors Thomas Cardon
+     */
+    class Fruit : public Item
+    {
+        private:
+            nsGui::Sprite sprite;
+
+        public:
+            Fruit(nsGraphics::Vec2D pos) : Item(pos), sprite("../GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/res/items/fruit_" + std::to_string(rand() % 4) + ".i2s") {}
+
+            /**
+             * @brief Loads food
+             * @fn void load();
+             */
+            void load() override; // On dirait que sans = 0 à la fin, Qt ne veut pas compiler parce qu'il considère que la fonction n'est pas définie (source: stackoverflow)
+
+            /**
+             * @brief Updates food
+             * @fn void update(unsigned delta);
+             */
+            void update(unsigned delta) override;
+
+            /**
+             * @brief Renders resources
+             * @fn void render(MinGL & window);
+             */
+            void render(MinGL & window) override;
+
+            /**
+             * @brief Gets item type
+             * @return ItemType.ITEM
+             */
+            ItemType getType() override {
+                return nsGame::ItemType::FRUIT;
+            };
+    };
+}
+
+#endif // FRUIT_H
