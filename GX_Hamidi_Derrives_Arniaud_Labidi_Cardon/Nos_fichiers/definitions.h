c6826e1503c43900e299d0941b00b37976a88a14
Thomas Cardon <thomas.cardon@pm.me>
2021-01-18 14:24:35 +0100

    Ajout du fichier définitions

---
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/definitions.h
@@ -0,0 +1,11 @@
+#ifndef DEFINITIONS_H
+#define DEFINITIONS_H
+
+#define RES_PATH std::string("../GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/res")
+
+#define SCORE_FOOD 35
+#define SCORE_FRUIT 200
+
+#define MENU_KEY A
+
+#endif // DEFINITIONS_H
