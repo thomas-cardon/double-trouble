26c566f4e31bb02b81062d5f013d3704db30fccf
ines hamidi <ines.hamidi@etu.univ-amu.fr>
2021-01-20 17:18:54 +0100

    monster update

--- GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/monster.cpp
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/monster.cpp
@@ -218,9 +218,6 @@ void Monster::render(MinGL &window) {
 #include "entity.h"
 #include "map.h"
 #include "cooldowns.h"
-
-#include <math.h>
-#include <vector>
 #include <random>
 
 using namespace nsGame;
@@ -228,7 +225,7 @@ using namespace nsGame;
 void Monster::update(unsigned delta, CMat & mat)
 {
     Cooldowns::updateCooldowns(delta);
-    MonsterMoves(mat);
+    MonsterMoves(mat) * CELL_SIZE;
 };
 
 void Monster::render(MinGL &window) {
