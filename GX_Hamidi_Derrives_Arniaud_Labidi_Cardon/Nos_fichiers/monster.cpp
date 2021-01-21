06537f9f9f54389acdc6a5f658236a4e8d7a2526
ines hamidi <ines.hamidi@etu.univ-amu.fr>
2021-01-21 17:34:59 +0100

    flee

--- GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/monster.cpp
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/monster.cpp
@@ -86,7 +86,60 @@ void Monster::update(unsigned delta, CMat & mat)
 
 <<<<<<< HEAD
 
+    else if (this->behaviourId == 3) // Behaviour : move in random circle
+{
+    unsigned x = this->getPosition().getX(), y = this->getPosition().getY();
 
+    //random circles
+    bool circleID = rand() % 1;
+
+    if (circleID == 1) //circle to the left
+    {
+        if ((x + 1 <= mat[y].size() - 1) && !this->inCollision(mat, x + 1, y))
+        {
+            this->pos.setX(x + 1);
+        };
+
+        else if ((y + 1 <= mat.size() - 1) && !this->inCollision(mat, x, y + 1))
+        {
+            this->pos.setY(y + 1);
+        };
+
+        else if ((x - 1 >= 0) && !this->inCollision(mat, x - 1, y))
+        {
+            this->pos.setX(x - 1);
+        };
+
+        else if ((y - 1 >= 0) && !this->inCollision(mat, x, y - 1))
+        {
+             this->pos.setY(y - 1);
+        };
+    }//fi
+
+    else
+    {
+
+        if ((x - 1 >= 0) && !this->inCollision(mat, x - 1, y))
+        {
+            this->pos.setX(x - 1);
+        }
+
+        else if ((y - 1 >= 0) && !this->inCollision(mat, x, y - 1))
+        {
+            this->pos.setY(y - 1);
+        }
+
+        else if ((x + 1 <= mat[y].size() - 1) && !this->inCollision(mat, x + 1, y))
+        {
+            this->pos.setX(x + 1);
+        }
+
+        else if ((y + 1 <= mat.size() - 1) && !this->inCollision(mat, x, y + 1))
+        {
+            this->pos.setY(y + 1);
+        }
+    }
+}//end of behaviour 3
 
 >>>>>>> main
     else if (false) { // this->behaviourId == 4) { // Behaviour 4 => Random
