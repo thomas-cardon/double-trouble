361d50eb99a5652bd30a5521ae67320bb41a01e4
ines hamidi <ines.hamidi@etu.univ-amu.fr>
2021-01-21 12:02:56 +0100

    Monster -> flees player ébauche
    déplacement inspiré du fantôme orange de pacmac qui va vers bas gauche si pacman trop proche, sinon fait des mvt random

--- GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/monster.cpp
+++ GX_Hamidi_Derrives_Arniaud_Labidi_Cardon/Nos_fichiers/monster.cpp
@@ -1,15 +1,20 @@
 /**
  * @file    monster.cpp
 <<<<<<< main
+<<<<<<< main
  * @authors Ines Hamidi, Thomas Cardon, Alexandre Arniaud
 =======
  * @author  Ines Hamidi
 >>>>>>> Refonte des classes
+=======
+ * @authors Ines Hamidi, Thomas Cardon
+>>>>>>> Monster -> flees player ébauche
  * @date    11 janvier 2020
  * @version 1.0
  * @brief   Définition des méthodes de la classe monster.h
  **/
 
+<<<<<<< main
 <<<<<<< main
 #include "type.h"
 
@@ -212,71 +217,131 @@ void Monster::render(MinGL &window) {
 #include <mingl/gui/sprite.h>
 #include <mingl/shape/circle.h>
 
+=======
+>>>>>>> Monster -> flees player ébauche
 #include "type.h"
 
 #include "monster.h"
-#include "entity.h"
-#include "map.h"
 #include "cooldowns.h"
-#include <random>
+
+#include "definitions.h"
 
 using namespace nsGame;
 
-void Monster::update(unsigned delta, CMat & mat)
-{
-    Cooldowns::updateCooldowns(delta);
-    MonsterMoves(mat);
-};
-
-void Monster::render(MinGL &window) {
-    // juste un affichage pour voir ou est le monstre
-    window << nsShape::Circle(this->getCoordinates(), 2, nsGraphics::RGBAcolor(255 / this->behaviourId, 255 / this->behaviourId, 255 / this->behaviourId));
+Monster::Monster(unsigned behaviourId) {
+    this->behaviourId = behaviourId;
 }
 
+std::string Monster::getEntityId() {
+    return "Monster" + std::to_string(this->behaviourId);
+}
 
-void Monster::MonsterMoves(CMat & mat)
+void Monster::spawn() {
+    this->pos.setX(1);
+    this->pos.setY(1 * behaviourId);
+}
+
+void goTo(nsGraphics::Vec2D newPos) {
+
+}
+
+void Monster::load() {
+    std::cout << "[Monster#" << getEntityId() + "] Loading" << std::endl;
+
+    this->movementSpeed = 0.35;
+    Cooldowns::createCooldown(getEntityId() + "_move", this->_getDelay());
+
+    for (int i = 1; i <= 6; i++) {
+        this->top.sprites.push_back(nsGui::Sprite(RES_PATH + "/entities/monsters/" + std::to_string(this->behaviourId) + "/" + std::to_string(this->behaviourId) + "-" + std::to_string(i) + ".i2s"));
+    }
+
+    this->spawn();
+    goTo(nsGraphics::Vec2D(15, 15));
+}
+
+// Inspiré de l'algorithme trouvé sur Wikipédia: https://fr.wikipedia.org/wiki/Algorithme_de_parcours_en_largeur
+void BreadthFirstSearch(CMat & mat, nsGraphics::Vec2D A) {
+    std::vector<nsGraphics::Vec2D> list;
+}
+
+void Monster::update(unsigned delta, CMat & mat)
 {
-    std::string Cd1;
-    Cooldowns::createCooldown(Cd1, 100);
+    canMove = Cooldowns::isCooldownOver(getEntityId() + "_move");
+    if (!canMove) return;
 
-    std::string Cd2;
-    Cooldowns::createCooldown(Cd2, 110);
+    std::cout << "[Monster#" << getEntityId() + "] Position: x= " << this->getPosition().getY() << ", y= " << this->getPosition().getY() << std::endl;
 
-
-    if (Cooldowns::isCooldownOver(Cd1) && (this->behaviourId == 1 || this->behaviourId == 2)) //Behaviour 1 : follow outer walls || Behaviour 2 : follow a little wall
+    if (this->behaviourId == 1) //Behaviour 1 : follow outer walls
     {
+        unsigned x = this->getPosition().getX(), y = this->getPosition().getY();
 
-        if (mat[this->getPosition().getY()][this->getPosition().getX() - 1] != '0') // MurGauche->bas
-        {
-            this->getPosition().setY(this->getPosition().getY() - 1);
+        if ((x + 1 <= mat[y].size() - 1) && !this->inCollision(mat, x + 1, y)) {
+            this->pos.setX(x + 1);
         }
-
-        if (mat[this->getPosition().getY() - 1][this->getPosition().getX()] != '0') // MurBas->droite
-        {
-            this->getPosition().setX(this->getPosition().getX() + 1);
+        else if ((x - 1 >= 0) && !this->inCollision(mat, x - 1, y)) {
+            this->pos.setX(x - 1);
         }
-
-        if (mat[this->getPosition().getY()][this->getPosition().getX() + 1] != '0') // MurDroite->haut
-        {
-            this->getPosition().setY(this->getPosition().getY() + 1);
+        else if ((y + 1 <= mat.size() - 1) && !this->inCollision(mat, x, y + 1)) {
+            this->pos.setY(y + 1);
         }
-
-        if (mat[this->getPosition().getY() + 1][this->getPosition().getX()] != '0') // MurHaut->gauche
-        {
-            this->getPosition().setX(this->getPosition().getX() - 1);
+        else if ((y - 1 >= 0) && !this->inCollision(mat, x, y - 1)) {
+            this->pos.setY(y - 1);
         }
     }
+    else if (this->behaviourId == 2) {
 
-    else if (this->behaviourId == 3) // Behaviour : flee the player
-    {
-        //randomly moves
-        this->getPosition().setX(this->getPosition().getX() + rand() % 1 -1);
-        this->getPosition().setY(this->getPosition().getY() + rand() % 1 -1);
     }
 
+    else if (this->behaviourId == 3) // Behaviour : if player close : flees to back right, else, make rounds
+    {
+        unsigned x = this->getPosition().getX(), y = this->getPosition().getY();
+
+        if (distance Player/monster > a){
+            //make rounds
+            this->pos.setX(x + 1);
+            //cooldown
+            this->pos.setY(y + 1);
+            //cooldown
+            this->pos.setX(x - 1);
+            //cooldown
+            this->pos.setY(y - 1);}
+
+        else {
+            this->pos.setX(x - 1);
+            this->pos.setY(y - 1);
+        }
+
+    }
+
+
+    else if (false) { // this->behaviourId == 4) { // Behaviour 4 => Random
+        int move = rand() % 4 + 1;
+        unsigned x = this->getPosition().getX(), y = this->getPosition().getY();
+        std::cout << move << std::endl;
+
+        if (move == 0 && (x + 1 <= mat[y].size() - 1) && !this->inCollision(mat, x + 1, y)) {
+            this->pos.setX(x + 1);
+        }
+        else if (move == 1 && (x - 1 >= 0) && !this->inCollision(mat, x - 1, y)) {
+            this->pos.setX(x - 1);
+        }
+        else if (move == 2 && (y + 1 <= mat.size() - 1) && !this->inCollision(mat, x, y + 1)) {
+            this->pos.setY(y + 1);
+        }
+        else if ((y - 1 >= 0) && !this->inCollision(mat, x, y - 1)) {
+            this->pos.setY(y - 1);
+        }
+    }
 };
 <<<<<<< main
 >>>>>>> Refonte des classes
 =======
 
+<<<<<<< main
 >>>>>>> random moves
+=======
+void Monster::render(MinGL &window) {
+    this->top.setPosition(this->getCoordinates());
+    this->top.render(window);
+}
+>>>>>>> Monster -> flees player ébauche
